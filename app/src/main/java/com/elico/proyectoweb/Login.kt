e4b9e978d5d06e78a5a38f98419a8539d204f39d
Gonzalo Ballesteros <elico1904@hotmail.com>
2021-03-15 15:05:04 -0600

    Initial commit

--- app/src/main/java/com/elico/proyectoweb/Login.kt
+++ app/src/main/java/com/elico/proyectoweb/Login.kt
@@ -5,15 +5,16 @@ import android.content.Intent
 import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
+import android.view.View
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
 
@@ -23,22 +24,18 @@ class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_login)
         leer()
-
         cardView_google.setOnClickListener {
             login()
         }
         txt1.setOnClickListener {
-            message("saber mas")
+            mostrarIntegrantes()
+        }
+        salir_integrantes.setOnClickListener {
+            ocultarIntegrantes()
         }
     }
-
-    private fun message(cadena: String) {
-        Toast.makeText(this, "${cadena}", Toast.LENGTH_LONG).show()
-    }
-
     private fun login() {
         val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
@@ -59,9 +56,7 @@ class Login : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
-
     }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == GOOGLE_SING_IN) {
@@ -72,21 +67,38 @@ class Login : AppCompatActivity() {
                     val credential = GoogleAuthProvider.getCredential(account.idToken, null)
                     FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener {
                         if (it.isSuccessful) {
-
+                            bd.collection("users").document(account.email.toString()).set(
+                                    hashMapOf("correo" to account.email.toString(),
+                                            "nombre" to account.givenName.toString(),
+                                            "user" to "U"))
                             val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
                             prefs.putString("email", account.email.toString())
                             prefs.putString("nombre", account.givenName.toString())
+                            prefs.putString("user", "U")
                             prefs.apply()
                             leer()
                         }
-
                     }
-
                 }
-            } catch (e: ApiException) {
-            }
-
+            } catch (e: ApiException) {}
         }
     }
 
+    private fun mostrarIntegrantes(){
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+        window.statusBarColor = ContextCompat.getColor(applicationContext, R.color.negro_true);
+        integrantes.visibility = View.VISIBLE
+    }
+    private fun ocultarIntegrantes(){
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+        window.statusBarColor = ContextCompat.getColor(applicationContext, R.color.negro_false);
+        integrantes.visibility = View.GONE
+    }
+
+    override fun onBackPressed() {
+        //super.onBackPressed()
+        if(integrantes.visibility == View.VISIBLE){
+            ocultarIntegrantes()
+        }
+    }
 }
--- app/src/main/java/com/elico/proyectoweb/fragments/Fragment02.kt
+++ app/src/main/java/com/elico/proyectoweb/fragments/Fragment02.kt
@@ -1,11 +1,13 @@
 package com.elico.proyectoweb.fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.elico.proyectoweb.R
+import kotlinx.android.synthetic.main.fragment_02.view.*
 
 class Fragment02 : Fragment() {
 
@@ -15,7 +17,15 @@ class Fragment02 : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
         var view = inflater.inflate(R.layout.fragment_02, container, false)
+        view.fragment02_mensaje.text = "${leer()}"
         return view
+    }
+
+    private fun leer():String{
+        val prefs = activity?.getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
+        var user: String? = prefs?.getString("user", "null")
+        var tipo = "Tipo de usuario: ";
+        return if(user.equals("U")){"${tipo}Normal"}else{"${tipo}Administrador"}
     }
 
 }
